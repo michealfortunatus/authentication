36984e218087391dbd76a0f00ea493b4d31f2402
fortune <esoifeoma1@gmail.com>
2026-02-06 12:21:54 +0100

    Update Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -487,12 +487,12 @@ const downloadCSV = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-1 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-8">
           <div className="bg-white p-6 rounded shadow h-80">
             <h2 className="font-semibold mb-4">Learner Status (Pie)</h2>
 
             <div className="h-[240px]">
-              <ResponsiveContainer width="50%" height="100%">
+              <ResponsiveContainer width="100%" height="100%">
                 <PieChart>
                   <Pie
                     data={chartData}
